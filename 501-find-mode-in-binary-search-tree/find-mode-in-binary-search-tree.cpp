54491d805fdf4b07b59765c34f1b3c60cb9dbe9d
Snehal180-ai <snehalshukla130@gmail.com>
2026-09-10 09:50:59 +0530

    Time: 0 ms (100.00%) | Memory: 24.1 MB (92.66%) - LeetSync

---
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    int prev = 0;
+    int currCount = 0;
+    int maxCount = 0;
+    bool first = true;
+
+    void inorder(TreeNode* root) {
+        if (root == NULL)
+            return;
+
+        inorder(root->left);
+
+        if (first || root->val != prev) {
+            currCount = 1;
+            first = false;
+        } else {
+            currCount++;
+        }
+
+        if (currCount > maxCount) {
+            maxCount = currCount;
+            ans.clear();
+            ans.push_back(root->val);
+        } else if (currCount == maxCount) {
+            ans.push_back(root->val);
+        }
+
+        prev = root->val;
+
+        inorder(root->right);
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        inorder(root);
+        return ans;
+    }
+};
